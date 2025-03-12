3749f1dfde322a92a14ad412c4999a9dc2a0777a
boymeetsmini <boymeetsmini@icloud.com>
2025-03-11 20:22:14 -0700

    on messageCreate

--- index.ts
+++ index.ts
@@ -29,18 +29,34 @@ async function cleanUrl(url: string): Promise<string> {
     // Find all whitelisted params for given URL's domain
     const parsedWl = await whitelistMatches(parsedDomain);
 
-    // Remove tracking parameters (e.g., utm_*)
-    const searchParams = new URLSearchParams(parsedUrl.search);
-    for (const key of searchParams.keys()) {
-        // delete any keys not in the whitelist for specified domain
-        if (parsedWl.indexOf(key) === -1) {
-            searchParams.delete(key);
+    // Handle wildcard (*) (skip parameter delete if * present)
+    if (parsedWl.indexOf('*') === -1) {
+        // Remove tracking parameters (e.g., utm_*)
+        const searchParams = new URLSearchParams(parsedUrl.search);
+        for (const key of searchParams.keys()) {
+            // delete any keys not in the whitelist for specified domain
+            if (parsedWl.indexOf(key) === -1) {
+                searchParams.delete(key);
+            }
         }
+        // Rebuild the URL without tracking parameters
+        parsedUrl.search = searchParams.toString();
     }
 
-    // Rebuild the URL without tracking parameters
-    parsedUrl.search = searchParams.toString();
     return parsedUrl.toString();
+}
+
+// Function to replace all URLs with cleaned ones
+function replaceUrls(message: string, origUrls: string[] | null, cleanedUrls: any[] | undefined): string {
+    let newMessage: string = message;
+
+    if (cleanedUrls === undefined) {
+        console.error('No URLs to clean');
+        return newMessage;
+    }
+
+    origUrls?.forEach((oUrl, idx) => newMessage = newMessage.replace(oUrl, cleanedUrls[idx]));
+    return newMessage;
 }
 
 // Create a new Discord client with the necessary intents
@@ -63,16 +79,17 @@ client.on('messageCreate', async (message: Message) => {
     if (message.author.bot) return;
 
     // Find URLs in the message using a regex
-    const urlRegex = /https?:\/\/[^\s]+/g;
+    const urlRegex = /http.?:\/\/[^\n\s\[\]\(\)]+/g;  // excludes all spaces, brackets and parenthesis after http:// or https://
     const urls: string[] | null = message.content.match(urlRegex);
     if (urls && urls.length === 0) return;
 
     // Clean the URLs
     const cleanedUrls = urls?.map(async (url) => await cleanUrl(url));
-    const newMessage: string = message.content.replace(urlRegex, '[url-$n]');
-    // TODO replace URL placeholders
-    // TODO send reconstructed message
+
+    // Replace URLs and edit message contents
+    message.edit(replaceUrls(message.content, urls, cleanedUrls));
 });
 
 // Log in to Discord with your bot token
+// TODO store token in launch config
 client.login('YOUR_DISCORD_BOT_TOKEN');
